4cf069dedb2297b0457faf4d0406ec447eefeef5
TreeMarch <m5c0traan@gmail.com>
2024-12-07 20:25:53 +0700

    admin

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 const pool = require("./db");
 
 const app = express();
@@ -8,10 +10,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// Cấu hình session
+app.use(
+  session({
+    secret: "tringtrong",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// cau hinh flash message
+app.use(flash());
+
+// Middleware truyền flash message vào tất cả các view
+app.use((req, res, next) => {
+  res.locals.successMessage = req.flash("success");
+  res.locals.errorMessage = req.flash("error");
+  next();
+});
 
 app.get("/", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM gasstation");
+    const result = await pool.query("SELECT * FROM gasstations");
     res.render("index", { gasStations: result.rows });
   } catch (err) {
     res.status(500).send(err.message);
@@ -22,7 +44,7 @@ app.get("/", async (req, res) => {
 app.get("/", async (req, res) => {
   try {
     const result = await pool.query(
-      "SELECT * FROM gasstation ORDER BY station_id"
+      "SELECT * FROM gasstations ORDER BY station_id"
     );
     res.render("index", { stations: result.rows });
   } catch (err) {
@@ -43,6 +65,8 @@ app.post("/add", async (req, res) => {
     station_latitude,
     station_longitude,
     brand,
+    types_of_gasoline,
+    oils,
     open_hours,
     services,
     contact_number,
@@ -50,21 +74,26 @@ app.post("/add", async (req, res) => {
   } = req.body;
   try {
     await pool.query(
-      "INSERT INTO gasstation (station_name, station_address, station_latitude,station_longitude,brand,open_hours,services,contact_number,status) VALUES ($1, $2, $3,$4,$5,$6,$7,$8,$9)",
+      "INSERT INTO gasstations (station_name, station_address, station_latitude,station_longitude,brand,types_of_gasoline,oils,open_hours,services,contact_number,status) VALUES ($1, $2, $3,$4,$5,$6,$7,$8,$9)",
       [
         station_name,
         station_address,
         station_latitude,
         station_longitude,
         brand,
+        types_of_gasoline,
+        oils,
         open_hours,
         services,
         contact_number,
         status,
       ]
     );
+    //flash message
+    req.flash("success", "Tạo thành công trạm xăng!");
     res.redirect("/");
   } catch (err) {
+    req.flash("error", "Đã xảy ra lỗi khi tạo trạm xăng.");
     res.status(500).send(err.message);
   }
 });
@@ -73,7 +102,7 @@ app.post("/add", async (req, res) => {
 app.get("/edit/:id", async (req, res) => {
   try {
     const result = await pool.query(
-      "SELECT * FROM gasstation WHERE station_id = $1",
+      "SELECT * FROM gasstations WHERE station_id = $1",
       [req.params.id]
     );
     res.render("edit", { station: result.rows[0] });
@@ -82,6 +111,7 @@ app.get("/edit/:id", async (req, res) => {
   }
 });
 
+// chon va chinh sua theo id
 app.put("/edit/:id", async (req, res) => {
   const {
     station_name,
@@ -89,6 +119,8 @@ app.put("/edit/:id", async (req, res) => {
     station_latitude,
     station_longitude,
     brand,
+    types_of_gasoline,
+    oils,
     open_hours,
     services,
     contact_number,
@@ -96,13 +128,15 @@ app.put("/edit/:id", async (req, res) => {
   } = req.body;
   try {
     await pool.query(
-      "UPDATE gasstation SET station_name = $1, station_address = $2, station_latitude = $3,station_longitude= $4,brand = $5 ,open_hours= $6,services = $7,contact_number = $8, status = $9 WHERE station_id = $10",
+      "UPDATE gasstations SET station_name = $1, station_address = $2, station_latitude = $3,station_longitude= $4,brand = $5,types_of_gasoline = $6,oils = $7 ,open_hours= $8,services = $9,contact_number = $10, status = $11 WHERE station_id = $12",
       [
         station_name,
         station_address,
         station_latitude,
         station_longitude,
         brand,
+        types_of_gasoline,
+        oils,
         open_hours,
         services,
         contact_number,
@@ -116,9 +150,10 @@ app.put("/edit/:id", async (req, res) => {
   }
 });
 
+// xoa theo id cay xang
 app.delete("/delete/:id", async (req, res) => {
   try {
-    await pool.query("DELETE FROM gasstation WHERE station_id = $1", [
+    await pool.query("DELETE FROM gasstations WHERE station_id = $1", [
       req.params.id,
     ]);
     res.redirect("/"); // Sau khi xóa, quay lại trang index
@@ -127,12 +162,12 @@ app.delete("/delete/:id", async (req, res) => {
   }
 });
 
-// Chi tiet
+// Chi tiet  cay xang
 app.get("/detail/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const result = await pool.query(
-      "SELECT * FROM gasstation WHERE station_id = $1",
+      "SELECT * FROM gasstations WHERE station_id = $1",
       [id]
     );
     if (result.rows.length === 0) {
@@ -147,3 +182,15 @@ app.get("/detail/:id", async (req, res) => {
 // Khởi động server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server chạy tại http://localhost:${PORT}`));
+
+// ===============================================================================
+
+// Trang User - Liệt kê các User
+app.get("/user", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users ");
+    res.render("user", { user: result.rows });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
